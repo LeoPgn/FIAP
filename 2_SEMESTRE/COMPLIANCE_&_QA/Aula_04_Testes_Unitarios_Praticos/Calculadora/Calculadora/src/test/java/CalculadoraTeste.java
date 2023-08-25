223c03b253fad2afb113a8f264c57c4eb3399e1e
Leonardo Paganini <leosonfire01@outlook.com>
2023-08-24 21:38:38 -0300

    Exemplos_Testes_Unitarios

---
+++ 2_SEMESTRE/COMPLIANCE_&_QA/Aula_04_Testes_Unitarios_Praticos/Calculadora/Calculadora/src/test/java/CalculadoraTeste.java
@@ -0,0 +1,59 @@
+import Calculadora.Calculadora;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculadoraTeste {
+
+    private Calculadora calc;
+
+    @BeforeEach
+    public void inicializar(){
+        this.calc = new Calculadora();
+    }
+    
+    @Test
+    void testeSomarDoisInteiros(){
+        //Calculadora calc = new Calculadora();
+
+        int a = 3;
+        int b = 2;
+        int resultado = calc.somar(a, b);
+
+        assertEquals(a+b, resultado);
+    }
+
+    @Test
+    void testeSubtrairInteiros(){
+        //Calculadora calc = new Calculadora();
+
+        int a = 3;
+        int b = 5;
+        int resultado = calc.subtrair(a, b);
+
+        assertEquals(a-b,resultado);
+    }
+
+    @Test
+    void testeMultiplicarInteiros(){
+        //Calculadora calc = new Calculadora();
+
+        int a = 3;
+        int b = 3;
+        int resultado = calc.multiplicar(a, b);
+
+        assertEquals(a*b, resultado);
+    }
+
+    @Test
+    void testDividirInteiros(){
+        //Calculadora calc = new Calculadora();
+
+        int a = 3;
+        int b = 3;
+        int resultado = calc.dividir(a, b);
+
+        assertEquals(a/b, resultado);
+    }
+
+}
