4b821db75d6e5079c3a3a90991b8231f683fd379
Leonardo Paganini <leosonfire01@outlook.com>
2023-09-28 19:09:28 -0300

    Aula_HybridMobile

---
+++ 2_SEMESTRE/HYBRID_MOBILE_APP_DEVELOPMENT/CP1/CP1_LeonardoPaganini_96562/cp1_mobile/App.js
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+
+
+import Instagram from './instagram/card';
+
+export default function App() {
+  return (
+    <View>
+      <Instagram/>
+    </View>
+  );
+}
