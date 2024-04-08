60cde1588b1cc851f7c763399c473196efe6b981
Leonardo Paganini <leosonfire01@outlook.com>
2024-04-08 12:00:57 -0300

    create_sprint3_DB
    In this commit I created the basis (tables, instructions, inserts) to start coding the project

---
+++ 3&4_SEMESTRE/CHALLENGE_LEVEL_GROUP/SPRINT_3/DATABASE/2TDSPS_2024_AeroPartsSql_RM96562_RM97305_RM97032.sql
@@ -0,0 +1,112 @@
+/*
+ 2TDSPS - Challenge Level Group - 3° Sprint - Grupo: AeroParts
+ Integrantes:
+ RM96562 - Leonardo Paganini
+ RM97032 - Regina Pompeo
+ RM97305 - Jhonn Brandon
+*/
+
+/*
+Passo 1 - Rode da linha 14 até a 19 para excluir as tabelas e garantir que o projeto rode de forma linear
+*/
+
+DROP TABLE usuario CASCADE CONSTRAINTS;
+DROP TABLE pedido CASCADE CONSTRAINTS;
+DROP TABLE item_pedido CASCADE CONSTRAINTS;
+DROP TABLE cotacao CASCADE CONSTRAINTS;
+DROP TABLE produto CASCADE CONSTRAINTS;
+DROP TABLE fornecedor CASCADE CONSTRAINTS;
+
+/*
+Passo 2 - Rode da linha 24 até a 65 para criar as tabelas
+*/
+
+CREATE TABLE usuario (
+    usuario_id    NUMBER(10) NOT NULL,
+    usuario_nome  VARCHAR2(50) NOT NULL,
+    usuario_email VARCHAR2(50) NOT NULL,
+    usuario_senha VARCHAR2(50) NOT NULL
+);
+
+CREATE TABLE pedido (
+    pedido_id          NUMBER(10) NOT NULL,
+    pedido_data        DATE NOT NULL,
+    pedido_status      VARCHAR2(50) NOT NULL,
+    usuario_usuario_id NUMBER(10) NOT NULL,
+    cotacao_cotacao_id NUMBER(10) NOT NULL
+);
+
+CREATE TABLE item_pedido (
+    item_id            NUMBER(10) NOT NULL,
+    item_quantidade    NUMBER(10) NOT NULL,
+    pedido_pedido_id   NUMBER(10) NOT NULL,
+    produto_produto_id NUMBER(10) NOT NULL
+);
+
+CREATE TABLE cotacao (
+    cotacao_id     NUMBER(10) NOT NULL,
+    cotacao_data   DATE NOT NULL,
+    preco_unitario FLOAT(10) NOT NULL
+);
+
+CREATE TABLE produto (
+    produto_id               NUMBER(10) NOT NULL,
+    produto_nome             VARCHAR2(50) NOT NULL,
+    produto_descricao        VARCHAR2(120) NOT NULL,
+    produto_preco            FLOAT(10) NOT NULL,
+    fornecedor_fornecedor_id NUMBER(10) NOT NULL
+);
+
+CREATE TABLE fornecedor (
+    fornecedor_id       NUMBER(10) NOT NULL,
+    fornecedor_nome     VARCHAR2(50) NOT NULL,
+    fornecedor_endereco VARCHAR2(50) NOT NULL,
+    fornecedor_contato  VARCHAR2(20) NOT NULL
+);
+
+/*
+Passo 3 - Rode da linha 71 até a 111 para inserir dados nas tabelas criadas anteriormente
+*/
+
+-- Inserindo dados na tabela 'usuario'
+INSERT INTO usuario (usuario_id, usuario_nome, usuario_email, usuario_senha) VALUES (1, 'João', 'joao@email.com', 'senha123');
+INSERT INTO usuario (usuario_id, usuario_nome, usuario_email, usuario_senha) VALUES (2, 'Maria', 'maria@email.com', 'senha456');
+INSERT INTO usuario (usuario_id, usuario_nome, usuario_email, usuario_senha) VALUES (3, 'Pedro', 'pedro@email.com', 'senha789');
+INSERT INTO usuario (usuario_id, usuario_nome, usuario_email, usuario_senha) VALUES (4, 'Ana', 'ana@email.com', 'senha101');
+INSERT INTO usuario (usuario_id, usuario_nome, usuario_email, usuario_senha) VALUES (5, 'Carlos', 'carlos@email.com', 'senha202');
+
+-- Inserindo dados na tabela 'fornecedor'
+INSERT INTO fornecedor (fornecedor_id, fornecedor_nome, fornecedor_endereco, fornecedor_contato)VALUES (1, 'PartsBase', 'Rua Miguel Cervantes Rodrigues', '(123) 456-7890');
+INSERT INTO fornecedor (fornecedor_id, fornecedor_nome, fornecedor_endereco, fornecedor_contato)VALUES (2, 'Boeing', 'Travessa Helma', '(555) 987-6543');
+INSERT INTO fornecedor (fornecedor_id, fornecedor_nome, fornecedor_endereco, fornecedor_contato)VALUES (3, 'Airbus', 'Avenida Nove de Julho', '(321) 789-0123');
+INSERT INTO fornecedor (fornecedor_id, fornecedor_nome, fornecedor_endereco, fornecedor_contato)VALUES (4, 'Honeywell Aerospace', 'Rua Nossa Senhora de Lourdes', '(888) 234-5678');
+INSERT INTO fornecedor (fornecedor_id, fornecedor_nome, fornecedor_endereco, fornecedor_contato)VALUES (5, 'GE Aviation', 'Rua Sim?o Carlos Pimenta', '(555) 123-4567');
+
+-- Inserindo dados na tabela 'cotacao'
+INSERT INTO cotacao (cotacao_id, cotacao_data, preco_unitario) VALUES (1, TO_DATE('2023-10-23', 'YYYY-MM-DD'), 100.0);
+INSERT INTO cotacao (cotacao_id, cotacao_data, preco_unitario) VALUES (2, TO_DATE('2023-10-22', 'YYYY-MM-DD'), 90.0);
+INSERT INTO cotacao (cotacao_id, cotacao_data, preco_unitario) VALUES (3, TO_DATE('2023-10-21', 'YYYY-MM-DD'), 80.0);
+INSERT INTO cotacao (cotacao_id, cotacao_data, preco_unitario) VALUES (4, TO_DATE('2023-10-20', 'YYYY-MM-DD'), 70.0);
+INSERT INTO cotacao (cotacao_id, cotacao_data, preco_unitario) VALUES (5, TO_DATE('2023-10-19', 'YYYY-MM-DD'), 60.0);
+
+-- Inserindo dados na tabela 'pedido'
+INSERT INTO pedido (pedido_id, pedido_data, pedido_status, usuario_usuario_id, cotacao_cotacao_id) VALUES (1, TO_DATE('2023-10-23', 'YYYY-MM-DD'), 'Em andamento', 1, 5);
+INSERT INTO pedido (pedido_id, pedido_data, pedido_status, usuario_usuario_id, cotacao_cotacao_id) VALUES (2, TO_DATE('2023-10-22', 'YYYY-MM-DD'), 'Concluído', 2, 4);
+INSERT INTO pedido (pedido_id, pedido_data, pedido_status, usuario_usuario_id, cotacao_cotacao_id) VALUES (3, TO_DATE('2023-10-21', 'YYYY-MM-DD'), 'Em andamento', 3, 3);
+INSERT INTO pedido (pedido_id, pedido_data, pedido_status, usuario_usuario_id, cotacao_cotacao_id) VALUES (4, TO_DATE('2023-10-20', 'YYYY-MM-DD'), 'Concluído', 4, 2);
+INSERT INTO pedido (pedido_id, pedido_data, pedido_status, usuario_usuario_id, cotacao_cotacao_id) VALUES (5, TO_DATE('2023-10-19', 'YYYY-MM-DD'), 'Em andamento', 5, 1);
+
+-- Inserindo dados na tabela 'produto'
+INSERT INTO produto (produto_id, produto_nome, produto_descricao, produto_preco, fornecedor_fornecedor_id) VALUES (1, 'Parafusos de Aeronave', 'Parafusos de alta resist?ncia usados na montagem e manuteção de aeronaves.', 2.30, 1);
+INSERT INTO produto (produto_id, produto_nome, produto_descricao, produto_preco, fornecedor_fornecedor_id) VALUES (2, 'Filtros de Combust?vel', 'Filtros que removem impurezas do combustível antes que ele entre no motor da aeronave.', 25.0, 2);
+INSERT INTO produto (produto_id, produto_nome, produto_descricao, produto_preco, fornecedor_fornecedor_id) VALUES (3, 'Baterias de Avião', 'Baterias de aeronaves para fornecer energia elétrica a sistemas e componentes eletrénicos.', 150.0, 3);
+INSERT INTO produto (produto_id, produto_nome, produto_descricao, produto_preco, fornecedor_fornecedor_id) VALUES (4, 'Velas de Ignição', 'Velas de ignição especiais para motores de aeronaves a pistão.', 10.0, 4);
+INSERT INTO produto (produto_id, produto_nome, produto_descricao, produto_preco, fornecedor_fornecedor_id) VALUES (5, 'Luzes de Navegação', 'Luzes de navegação usadas para indicar a posição e direção de uma aeronave durante o voo.', 30.0, 5);
+
+-- Inserindo dados na tabela 'item_pedido'
+INSERT INTO item_pedido (item_id, item_quantidade, pedido_pedido_id, produto_produto_id) VALUES (1, 87, 1, 5);
+INSERT INTO item_pedido (item_id, item_quantidade, pedido_pedido_id, produto_produto_id) VALUES (2, 27, 2, 4);
+INSERT INTO item_pedido (item_id, item_quantidade, pedido_pedido_id, produto_produto_id) VALUES (3, 72, 3, 3);
+INSERT INTO item_pedido (item_id, item_quantidade, pedido_pedido_id, produto_produto_id) VALUES (4, 15, 4, 2);
+INSERT INTO item_pedido (item_id, item_quantidade, pedido_pedido_id, produto_produto_id) VALUES (5, 65, 5, 1);
+
